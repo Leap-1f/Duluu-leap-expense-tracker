e6e8765303be471393526fc4511aedd15f557f6c
billduluu <billduluu@gmail.com>
2024-03-04 19:28:32 +0800

    3 sariin 4

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+import express from "express";
+import { sql } from "./config/database.js";
+import { user } from "./src/router/user.js";
+const app = express();
+const port = 3000;
+
+app.use("/users", user);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
